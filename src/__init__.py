d01ac59ae808053350f3830a9803c71ad268997a
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-08 00:31:10 +0900

    -m

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,7 @@
 
 Import the `feed` module to work with the Real Python feed:
 
-    >>> from reader import feed
-    >>> feed.get_titles()
+
     ['Logging in Python', 'The Best Python Books', ...]
 
 See https://github.com/realpython/reader/ for more information.
--- src/mainPyPIHelloWorld.py
+++ src/mainPyPIHelloWorld.py
@@ -1,3 +1,3 @@
-from first import *
+from pyPPHelloWorld import *
 print("add", add(2, 3))
 print("sub", sub(2, 3))
