d1b1f039930c38b92f550d641e5f97e22211f7f1
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-08 00:43:01 +0900

    -m

--- src/__init__.py
+++ src/__init__.py
@@ -18,7 +18,7 @@ except ModuleNotFoundError:
 
 
 # Version of realpython-reader package
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 # Read URL of the Real Python feed from config file
 _cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
